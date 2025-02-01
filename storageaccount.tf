c7a45742e37f63439e477772b7e321def33a1242
ajirijames <ajirijames1@gmail.com>
2025-01-31 20:26:58 -0500

    Update storageaccount.tf

--- storageaccount.tf
+++ storageaccount.tf
@@ -4,7 +4,7 @@ resource "azurerm_resource_group" "ajiri24" {
 }
 
 resource "azurerm_storage_account" "example" {
-  name                     = "storageaccountname"
+  name                     = "ajirijames"
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"

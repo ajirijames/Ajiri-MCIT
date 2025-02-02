ce515e00b890bdbc52d2dcf1bb601d6f92caf2ec
ajirijames <ajirijames1@gmail.com>
2025-02-01 21:44:21 -0500

    Update resourcegroup.tf

--- resourcegroup.tf
+++ resourcegroup.tf
@@ -2,3 +2,7 @@ resource "azurerm_resource_group" "ajirimcit24" {
   name     = "ajirimcit24_resource_group_ajiri24"
   location = "canadacentral"
 }
+resource "azurerm_resource_group" "ajirimcit25" {
+  name     = "ajirimcit25_resource_group_ajiri"
+  location = "canadacentral"
+}

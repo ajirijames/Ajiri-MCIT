0680bdb9ff6a427538ff5a2dec9e0c7449fbb71a
ajirijames <ajirijames1@gmail.com>
2025-02-03 17:08:00 -0500

    Update storageaccount.tf

--- storageaccount.tf
+++ storageaccount.tf
@@ -1,10 +1,10 @@
-resource "azurerm_resource_group" "ajiri24" {
+resource "azurerm_resource_group" "ajirimcit24" {
   name     = "ajiri24"
   location = "Canada Central"
 }
 
 resource "azurerm_storage_account" "example" {
-  name                     = "ajirijames"
+  name                     = "ajiristorage"
   resource_group_name      = azurerm_resource_group.ajirimcit24.name
   location                 = azurerm_resource_group.canadacentral.location
   account_tier             = "Standard"
